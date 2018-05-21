2d5095c03dbd3fbcaff6a1f0f408a8755c609fc5
KParhar <coderk2001@gmail.com>
2018-05-21 13:36:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+from lxml import html
+
+loginLink = "https://www.fitbit.com/en-ca/login"
+username = "tigrambazzi@gmail.com"
+password = "Bhupinder23"
+
+loginPayload = {
+	"email": username
+	"password": password
+	"csrfmiddlewaretoken": "c4359d4fcc1c437d8250117845e63a0d"
+}
+
+link = "https://www.fitbit.com/"
+website = requests.get(link)
+htmlText = website.text
+
+print(htmlText)
