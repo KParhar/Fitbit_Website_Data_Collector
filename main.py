82681472775522f5c86c9494b12cb63797d25da2
KParhar <coderk2001@gmail.com>
2018-05-21 15:56:41 -0400

    2018-05-21 3:56PM

--- main.py
+++ main.py
@@ -2,17 +2,54 @@ import requests
 from lxml import html
 
 loginLink = "https://www.fitbit.com/en-ca/login"
-username = "tigrambazzi@gmail.com"
+
+sRequests = requests.session()
+result = sRequests.get(loginLink)
+htmlLogin = html.fromstring(result.text)
+
+login = list(set(htmlLogin.xpath("//input[@name='login']/@value")))[0]
+includeWorkflow = ""
+redirect = ""
+switchToNonSecureOnRedirect = list(set(htmlLogin.xpath("//input[@name='switchToNonSecureOnRedirect']/@value")))[0]
+csrfToken = list(set(htmlLogin.xpath("//input[@name='csrfToken']/@value")))[0]
+disableThirdPartyLogin = list(set(htmlLogin.xpath("//input[@name='disableThirdPartyLogin']/@value")))[0]
+email = "tigrambazzi@gmail.com"
 password = "Bhupinder23"
+rememberMe = "true"
+_sourcePage = list(set(htmlLogin.xpath("//input[@name='_sourcePage']/@value")))[0]
+__fp = list(set(htmlLogin.xpath("//input[@name='__fp']/@value")))[0]
 
 loginPayload = {
-	"email": username
-	"password": password
-	"csrfToken": "c4359d4fcc1c437d8250117845e63a0d"
+	"login" : login,
+	"includeWorkflow" : includeWorkflow,
+	"redirect" : redirect,
+	"switchToNonSecureOnRedirect" : switchToNonSecureOnRedirect,
+	"csrfToken" : csrfToken,
+	"disableThirdPartyLogin" : disableThirdPartyLogin,
+	"email" : email,
+	"password" : password,
+	"rememberMe" : rememberMe,
+	"_sourcePage" : _sourcePage,
+	"__fp" : __fp
 }
 
-link = "https://www.fitbit.com/"
-website = requests.get(link)
-htmlText = website.text
+result = sRequests.post(
+	loginLink, 
+	data = loginPayload, 
+	headers = dict(referer=loginLink)
+)
 
-print(htmlText)
+print(result.status_code)
+
+link = "https://www.fitbit.com/"
+
+result = sRequests.get(link, headers = dict(referer = link))
+htmlText = result.text
+
+link = "https://www.fitbit.com/"
+
+result = sRequests.get(link)
+htmlText = result.text
+
+
+print(result.url)
